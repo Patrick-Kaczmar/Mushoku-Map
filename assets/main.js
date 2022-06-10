25c61610819cd26180231d0b3c2a582aacefe2ae
Patrick Kaczmar <pk1blue@yahoo.com>
2022-06-10 04:43:16 -0400

    added conditional to check if a square was used already

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,25 @@
 let counter = 0;
 
+function reset() {
+    location.reload()
+}
+
 function doTheThing (idNumber) {
-    if (counter === 0) {
+    let chosen = document.getElementById(idNumber);
+    if (counter === 0 && !chosen.classList.contains("old")) {
         let x = document.createElement("img");
         x.src = "./assets/images/1024px-Red_X.svg.png";
-        let chosen = document.getElementById(idNumber);
+        chosen = document.getElementById(idNumber);
         chosen.appendChild(x);
-        counter += 1
-    } else {
+        counter += 1;
+        chosen.classList.add("old");
+    } else if (counter === 1 && !chosen.classList.contains("old")) {
         let o = document.createElement("img")
         o.src = "./assets/images/letter O.png";
-        let chosen = document.getElementById(idNumber);
+        chosen = document.getElementById(idNumber);
         chosen.appendChild(o);
         counter -= 1;
+        chosen.classList.add("old");
     }
 }
+
